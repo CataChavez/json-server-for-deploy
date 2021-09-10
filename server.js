bc4c90c2b37d1aa4261a53b48f53bb51a4cfac3f
Catty <kata.chavez@gmail.com>
2021-09-09 22:41:14 -0300

    jwt add

--- server.js
+++ server.js
@@ -1,10 +1,26 @@
 const jsonServer = require('json-server');
+const auth = require('json-server-auth')
+
 const server = jsonServer.create();
-const router = jsonServer.router('db.json');
-const middlewares = jsonServer.defaults();
+const router = jsonServer.router('db.json')
+const middlewares = jsonServer.defaults()
 const port = process.env.PORT || 4000;
 
-server.use(middlewares);
-server.use(router);
+server.use(middlewares)
 
-server.listen(port);
+server.db = router.db
+
+const rules = auth.rewriter({
+    // Permission rules
+    users: 640,
+    products: 644,
+    store: 644
+
+})
+
+server.use(rules)
+server.use(auth)
+server.use(router)
+
+//Servidor
+server.listen(port, () => console.log(`API REST ON PORT ${port}`));
