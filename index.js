f970f9a5ad14e7a8000b37590b7d109b8fab95fc
Catty <kata.chavez@gmail.com>
2021-09-19 23:49:50 -0300

    auth

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const jsonServer = require('json-server');
-//const auth = require('json-server-auth')
+const auth = require('json-server-auth')
 
 const server = jsonServer.create();
 const router = jsonServer.router('db.json')
@@ -22,7 +22,7 @@ server.db = router.db */
 //})
 
 //server.use(rules)
-//server.use(auth)
+server.use(auth)
 server.use(router)
 
 //Servidor
